8a69264c7acdedf3a6f1e800afd7f875a7c8d1b2
matheuz233 <matheusaugus233@gmail.com>
2025-03-22 15:08:02 -0300

    feat: :sparkles: Configurações Iniciais do Projeto

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3795d3f94b4dc0206f230a511f1b2adcf0f7bd91',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -422,9 +422,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3795d3f94b4dc0206f230a511f1b2adcf0f7bd91',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
